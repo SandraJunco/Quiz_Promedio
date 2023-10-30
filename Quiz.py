c18957d6be669a036775112a3aa0502105a35088
Sandra Junco <smjuncop@udistrital.edu.co>
2023-10-30 18:32:36 -0500

    Commit_02

--- Quiz.py
+++ Quiz.py
@@ -1,7 +1,9 @@
+# Código Que calcula la nota acomulada del semestre
+
 lista=[50,50,45,48,0,0]
 porcentaje=[0.05,0.1,0.2,0.06,0.06,0.03]
-
 notafinal=0
+
 for i in range(len(lista)):
     nota= lista[i]*porcentaje[i]
     notafinal=notafinal+nota
