c29bbcc7d794ada33c25a4ff30621e99ab87f030
Sandra Junco <smjuncop@udistrital.edu.co>
2023-10-30 18:28:03 -0500

    Commit_01

---
+++ Quiz.py
@@ -0,0 +1,8 @@
+lista=[50,50,45,48,0,0]
+porcentaje=[0.05,0.1,0.2,0.06,0.06,0.03]
+
+notafinal=0
+for i in range(len(lista)):
+    nota= lista[i]*porcentaje[i]
+    notafinal=notafinal+nota
+print ('Nota acomulada =',notafinal)
